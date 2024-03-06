055733dc65a45c9190d659bba03de4f296e9a24c
SorokaSofia <s.soroka31@knu.ua>
2024-03-06 17:14:07 +0200

    Function to move the player based on the allowed turns

--- pacman.py
+++ pacman.py
@@ -241,3 +241,15 @@ def check_position(centerx, centery):
         turns[1] = True
 
     return turns
+
+def move_player(play_x, play_y):
+   
+    if direction == 0 and turns_allowed[0]:
+        play_x += player_speed
+    elif direction == 1 and turns_allowed[1]:
+        play_x -= player_speed
+    if direction == 2 and turns_allowed[2]:
+        play_y -= player_speed
+    elif direction == 3 and turns_allowed[3]:
+        play_y += player_speed
+    return play_x, play_y
