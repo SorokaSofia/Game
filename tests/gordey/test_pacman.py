6152f82a468131989ae733b332f855bc6b37c088
GordiyKrizanovskiy <gordiy.krizanovskiy@knu.ua>
2024-04-25 23:07:35 +0300

    Test Ghost Movement in Scatter Mode

--- tests/gordey/test_pacman.py
+++ tests/gordey/test_pacman.py
@@ -11,3 +11,10 @@ def test_ghost_movement():
     # Change mode to frightened
     ghost.mode = "frightened"
     assert ghost.move(player_position) == (3, 3), "Ghost should move away from the player"
+
+    # Change mode to scatter
+    ghost.mode = "scatter"
+    # Randomly moving, we can't predict exactly where, but we can check if it moved
+    initial_position = ghost.position
+    new_position = ghost.move(player_position)
+    assert new_position != initial_position, "Ghost should move randomly"
