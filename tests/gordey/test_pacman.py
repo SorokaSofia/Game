27f2a67f8c7f3ff47d0bcf3ee3e802e9a7243b62
GordiyKrizanovskiy <gordiy.krizanovskiy@knu.ua>
2024-04-25 23:06:05 +0300

    Test Ghost Movement Towards Player

--- tests/gordey/test_pacman.py
+++ tests/gordey/test_pacman.py
@@ -4,3 +4,6 @@ from game_module import Ghost
 def test_ghost_movement():
     player_position = (5, 5)
     ghost = Ghost("Blinky", "normal", (3, 3))
+
+    # Move towards player
+    assert ghost.move(player_position) == (4, 4), "Ghost should move towards the player"
