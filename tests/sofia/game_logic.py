61da3924f6cb0a60fc0fe4420451f98aa3a0e3de
GordiyKrizanovskiy <gordiy.krizanovskiy@knu.ua>
2024-04-26 19:47:24 +0300

    /

--- tests/sofia/game_logic.py
+++ tests/sofia/game_logic.py
@@ -14,13 +14,17 @@ def move_player(x, y, direction, turns_allowed, player_speed):
     Returns:
         tuple: The new x and y coordinates of the player after movement.
     """
-    if direction == 0 and turns_allowed[0]:  # Right
+    if direction == 0 and \
+    turns_allowed[0]:  # Right
         x += player_speed
-    elif direction == 1 and turns_allowed[1]:  # Left
+    elif direction == 1 and \
+        turns_allowed[1]:  # Left
         x -= player_speed
-    elif direction == 2 and turns_allowed[2]:  # Up
+    elif direction == 2 and \
+        turns_allowed[2]:  # Up
         y -= player_speed
-    elif direction == 3 and turns_allowed[3]:  # Down
+    elif direction == 3 and \
+        turns_allowed[3]:  # Down
         y += player_speed
 
     return x, y
