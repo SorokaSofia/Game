b8ac2c9ce6a63aeb21a973a18963db553802024a
Alexei Gopkalo <vn.gopkalo1978@gmail.com>
2024-04-26 18:15:09 +0300

    rework font

--- tests/yura/test_pacman.py
+++ tests/yura/test_pacman.py
@@ -6,22 +6,13 @@ import os
 # Function to initialize Pygame, which can be used in tests
 def init_pygame():
     pygame.init()
-    pygame.mixer.init()
 
 @pytest.fixture
 def resource_setup():
     init_pygame()
     font = pygame.font.Font(None, 48)
-    menu_sound = pygame.mixer.Sound('assets/Paramind_cotton_eye_joe_mashup.mp3')
-    return font, menu_sound
+    return font
 
-def test_font_loaded(resource_setup):
-    font, _ = resource_setup
-    assert isinstance(font, pygame.font.Font), "Font is not loaded"
-
-def test_sound_loaded(resource_setup):
-    _, menu_sound = resource_setup
-    assert isinstance(menu_sound, pygame.mixer.Sound), "Sound is not loaded"
 
 def test_background_image_loading():
     init_pygame()
