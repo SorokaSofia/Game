47e17574d8c0ee12d201c2e83a89238a67fe9f30
GordiyKrizanovskiy <gordiy.krizanovskiy@knu.ua>
2024-04-26 19:57:12 +0300

    flake

--- tests/gordey/game_module.py
+++ tests/gordey/game_module.py
@@ -1,10 +1,13 @@
 from random import randint
 
 class Ghost:
+
+
     def __init__(self, name, mode, position):
         self.name = name
         self.mode = mode
         self.position = position
+
 
     def move(self, player_position):
         if self.mode == 'scatter':
@@ -13,9 +16,11 @@ class Ghost:
             return self.move_away_from(player_position)
         return self.move_towards(player_position)
 
+
     def move_randomly(self):
         self.position = (randint(0, 10), randint(0, 10))
         return self.position
+
 
     def move_towards(self, player_position):
         ghost_x, ghost_y = self.position
@@ -24,6 +29,7 @@ class Ghost:
         ghost_y += 1 if ghost_y < player_y else -1
         self.position = (ghost_x, ghost_y)
         return self.position
+
 
     def move_away_from(self, player_position):
         ghost_x, ghost_y = self.position
